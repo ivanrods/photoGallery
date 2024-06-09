bcd8e855bef8b338500c101e2a7d7809a5269557
ivan <contaivanrodrigues@gmail.com>
2024-06-09 11:50:58 -0300

    alteraçoes

--- src/elements.js
+++ src/elements.js
@@ -2,10 +2,19 @@ export const figures = document.querySelector("article")
 
 export const navbar = document.querySelector("nav")
 export const search = document.querySelector(".bar")
+export const explorer = document.querySelector(".explorer button")
 export const iconSearch = document.querySelector(".icon-search-hide")
 export let pg = document.querySelector(".logo .logo-pg")
 export let photo = document.querySelector(".logo-photo")
 
-export const aderir = document.querySelector("ul .aderir")
+export const aderir = document.querySelector(".aderir")
+export const login = document.querySelector(".login")
 export const modal = document.querySelector(".modal-wrapper")
 export const closeModal = document.querySelector(".close")
+export const body = document.querySelector("body")
+
+export const menu = document.querySelector(".menu")
+export const content = document.querySelector(".content")
+export const closeContent = document.querySelector(".content .close")
+
+export const load = document.querySelector(".load")
--- src/events.js
+++ src/events.js
@@ -1,29 +1,55 @@
-import { navbar, search, iconSearch, pg, photo ,aderir, modal, closeModal } from "./elements.js"
+import * as el from "./elements.js"
 
 //evento navbar
 export const scroll = document.addEventListener("scroll", function() {
   var posicaoy = window.pageYOffset;
   if(!(posicaoy == 0)){
-    navbar.classList.add('navScroll')
-    search.style.display = 'flex'
-    iconSearch.style.display = 'flex'
-    pg.src = "./assets/fg-dark.svg"
-    photo.src = "./assets/Photo-Gallery-dark.svg"
+    el.navbar.classList.add('navScroll')
+    el.explorer.style.color = "#636363"
+    el.search.style.display = 'flex'
+    el.iconSearch.style.display = 'flex'
+    el.pg.src = "./assets/fg-dark.svg"
+    el.photo.src = "./assets/Photo-Gallery-dark.svg"
     
   }else{
-    navbar.classList.remove('navScroll')
-    search.style.display = 'none'
-    iconSearch.style.display = 'none'
-    pg.src = "./assets/fg.svg"
-    photo.src = "./assets/Photo-Gallery.svg"
+    el.navbar.classList.remove('navScroll')
+    el.explorer.style.color = "white"
+    el.search.style.display = 'none'
+    el.iconSearch.style.display = 'none'
+    el.pg.src = "./assets/fg.svg"
+    el.photo.src = "./assets/Photo-Gallery.svg"
   }
 });
 
 
-aderir.addEventListener('click', ()=>{
-  modal.classList.remove('hide2')
+el.aderir.addEventListener('click', ()=>{
+  el.modal.classList.remove('hide2')
+  el.body.style.overflow = "hidden";
  
 })
-closeModal.addEventListener('click', ()=> {
-  modal.classList.add('hide2')
+el.login.addEventListener('click', ()=>{
+  el.modal.classList.remove('hide2')
+  el.body.style.overflow = "hidden";
+ 
+})
+el.load.addEventListener('click', ()=>{
+  el.modal.classList.remove('hide2')
+  el.body.style.overflow = "hidden";
+ 
+})
+el.closeModal.addEventListener('click', ()=> {
+  el.modal.classList.add('hide2')
+  el.body.style.overflow = "auto";
+  
+})
+
+el.menu.addEventListener('click', ()=>{
+  el.content.classList.remove('hide2')
+})
+el.closeContent.addEventListener('click', ()=>{
+  el.content.classList.add('hide2')
+})
+
+el.explorer.addEventListener('click', ()=>{
+  
 })
